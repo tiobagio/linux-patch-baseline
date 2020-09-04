2dfe93f545e9db544190f1f8ddafb8c398be6a14
tiobagio <tio.bagio@gmail.com>
2020-09-04 23:16:48 +0800

    commit

---
+++ controls/patches.rb
@@ -0,0 +1,50 @@
+# encoding: utf-8
+# copyright: 2016, Christoph Hartmann
+# copyright: 2016, Dominik Richter
+# license: MPLv2
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+control 'verify-patches' do
+  impact 0.3
+  title 'Operating system is up-to-date'
+  describe linux_update do
+    it { should be_uptodate }
+  end
+end
+
+control 'patches' do
+  impact 0.3
+  title 'All operating system package updates are installed'
+  linux_update.updates.each { |update|
+    describe package(update['name']) do
+      its('version') { should eq update['version'] }
+    end
+  }
+  only_if { linux_update.updates.length > 0 }
+end
+
+control 'available-security-updates' do
+  impact 0.3
+  title 'All updates available for installed packages that are security relevant'
+  linux_update.sec_updates.each { |update|
+    str = update['name']
+    name = str.gsub(/[.].*/,'')
+    describe package(name) do
+      its('version') { should eq update['version'] }
+    end
+  }
+  only_if { linux_update.sec_updates.length > 0 }
+end
+
+control 'os-patches' do
+  impact 0.3
+  title 'All operating system patches are installed'
+  linux_update.patches.each do |patch|
+    describe patch do
+      it { should be_nil }
+    end
+  end
+end
